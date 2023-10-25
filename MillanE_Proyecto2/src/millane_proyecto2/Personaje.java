0937604f8fbb679120533df10e626e74dda78731
modosenin0 <97416036+modosenin0@users.noreply.github.com>
2023-10-24 20:00:25 -0400

    cola y personaje
    Falta determinar la calidad de personaje

---
+++ MillanE_Proyecto2/src/millane_proyecto2/Personaje.java
@@ -0,0 +1,201 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package millane_proyecto2;
+
+/**
+ *
+ * @author leste
+ */
+public class Personaje {
+    private int idNumber;
+    private int priority;  
+    private int contador; //inanicion
+    private String nameGames;  //nombre de juego mamaguevo millan
+    private char idLetter;  //Id que se va a desplegar en la UI
+    
+    private boolean abilitiesQuality;
+    private boolean hpQuality;
+    private boolean forceQuality;
+    private boolean agilityQuality;
+    private int individualQualityCounter;
+    
+    private int randomNumber;
+    
+    public Personaje(int counterPersonajes, String nameGames) {
+        this.idNumber = counterPersonajes;
+        this.idLetter = nameGames.charAt(0);
+        this.priority = 0;
+        this.contador = 0;
+        this.abilitiesQuality = false;
+        this.hpQuality = false;
+        this.forceQuality = false;
+        this.agilityQuality = false;
+        this.individualQualityCounter = 0;
+        
+    }
+    
+    public String getId() {
+        String result = this.idLetter + (String.valueOf(this.idNumber));
+        return result;
+    }
+    
+    public void printCarId() {
+        System.out.println(this.getId());
+    }
+
+    /**
+     * @return the idNumber
+     */
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    /**
+     * @param idNumber the idNumber to set
+     */
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    /**
+     * @return the priority
+     */
+    public int getPriority() {
+        return priority;
+    }
+
+    /**
+     * @param priority the priority to set
+     */
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    /**
+     * @return the contador
+     */
+    public int getContador() {
+        return contador;
+    }
+
+    /**
+     * @param contador the contador to set
+     */
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+
+    /**
+     * @return the nameGames
+     */
+    public String getNameGames() {
+        return nameGames;
+    }
+
+    /**
+     * @param nameGames the nameGames to set
+     */
+    public void setNameGames(String nameGames) {
+        this.nameGames = nameGames;
+    }
+
+    /**
+     * @return the idLetter
+     */
+    public char getIdLetter() {
+        return idLetter;
+    }
+
+    /**
+     * @param idLetter the idLetter to set
+     */
+    public void setIdLetter(char idLetter) {
+        this.idLetter = idLetter;
+    }
+
+    /**
+     * @return the abilitiesQuality
+     */
+    public boolean isAbilitiesQuality() {
+        return abilitiesQuality;
+    }
+
+    /**
+     * @param abilitiesQuality the abilitiesQuality to set
+     */
+    public void setAbilitiesQuality(boolean abilitiesQuality) {
+        this.abilitiesQuality = abilitiesQuality;
+    }
+
+    /**
+     * @return the hpQuality
+     */
+    public boolean isHpQuality() {
+        return hpQuality;
+    }
+
+    /**
+     * @param hpQuality the hpQuality to set
+     */
+    public void setHpQuality(boolean hpQuality) {
+        this.hpQuality = hpQuality;
+    }
+
+    /**
+     * @return the forceQuality
+     */
+    public boolean isForceQuality() {
+        return forceQuality;
+    }
+
+    /**
+     * @param forceQuality the forceQuality to set
+     */
+    public void setForceQuality(boolean forceQuality) {
+        this.forceQuality = forceQuality;
+    }
+
+    /**
+     * @return the agilityQuality
+     */
+    public boolean isAgilityQuality() {
+        return agilityQuality;
+    }
+
+    /**
+     * @param agilityQuality the agilityQuality to set
+     */
+    public void setAgilityQuality(boolean agilityQuality) {
+        this.agilityQuality = agilityQuality;
+    }
+
+    /**
+     * @return the individualQualityCounter
+     */
+    public int getIndividualQualityCounter() {
+        return individualQualityCounter;
+    }
+
+    /**
+     * @param individualQualityCounter the individualQualityCounter to set
+     */
+    public void setIndividualQualityCounter(int individualQualityCounter) {
+        this.individualQualityCounter = individualQualityCounter;
+    }
+
+    /**
+     * @return the randomNumber
+     */
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+
+    /**
+     * @param randomNumber the randomNumber to set
+     */
+    public void setRandomNumber(int randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+}
---
+++ MillanE_Proyecto2/src/millane_proyecto2/Queue.java
@@ -0,0 +1,157 @@
+package millane_proyecto2;
+
+/**
+ *
+ * @author leste
+ */
+
+public class Queue {
+    int lenght;
+    Personaje[] items;
+    int front;
+    int rear;
+
+    public Queue(int lenght) {
+        this.lenght = lenght;
+        this.items = new Personaje[lenght];
+        this.front = -1;
+        this.rear = -1;
+    }
+
+    public boolean isFull() {
+        return rear == lenght - 1;
+    }
+
+    public boolean isEmpty() {
+        return front == -1 && rear == -1;
+    }
+
+    public void enQueue(Personaje itemValue) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+        } else if (isEmpty()) {
+            front = rear = 0;
+            items[rear] = itemValue;
+        } else {
+            rear++;
+            items[rear] = itemValue;
+        }
+    }
+
+    public Personaje deQueue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return null;
+        } else if (front == rear) {
+            Personaje nodo = items[front];
+            front = rear = -1;
+            return nodo;
+        } else {
+            Personaje nodo = items[front];
+            for (int i = front; i < rear; i++) {
+                items[i] = items[i + 1];
+            }
+            rear--;
+            return nodo;
+        }
+        
+    }
+    
+    public int size() {
+        if (isEmpty()) {
+            return 0;
+        } else {
+            return rear - front + 1;
+        }
+    }
+
+//    public void display() {
+//        if (isEmpty()) {
+//            System.out.println("Queue is empty, underflow condition!!");
+//        } else {
+//            for (int i = front; i <= rear; i++) {
+//                System.out.println(items[i]);
+//            }
+//        }
+//    }
+    
+    public void displayNodes() {
+        if (isEmpty()) {
+            System.out.println("La cola esta vacia, underflow!!");
+        } else {
+            for (int i = front; i <= rear; i++) {
+                if (items[i] != null) {
+                    items[i].printCarId();
+                } else {
+                    System.out.println("hay null");
+                }
+            }
+        }
+    }
+    
+    public String displayQueue() {
+        String result = "La cola esta vacia";
+        if (!isEmpty()) {
+            
+            result = "";
+            for (int i = front; i <= rear; i++) {
+                if (items[i] != null) {
+                    if (i == 0) {
+                        result += items[i].getId();
+                    } else {
+                        result += "," + items[i].getId();
+                    }
+                } else {
+                    System.out.println("hay null"); //revisar
+                }
+            }
+        } 
+        return result;
+    }
+
+    public Personaje getPeak() {
+        if (isEmpty()) {
+            System.out.println("La cola esta vacia");
+            return null;
+        } else {
+            return items[front];
+        }
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+
+    public Personaje[] getItems() {
+        return items;
+    }
+
+    public void setItems(Personaje[] items) {
+        this.items = items;
+    }
+
+    public int getFront() {
+        return front;
+    }
+
+    public void setFront(int front) {
+        this.front = front;
+    }
+
+    public int getRear() {
+        return rear;
+    }
+
+    public void setRear(int rear) {
+        this.rear = rear;
+    }
+    
+    
+}
+
+
+
